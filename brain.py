895d04153296bcb618731cb38266c296cd21987e
Nilasis sen <sen.nilasis90@yahoo.com>
2014-11-19 16:57:51 -0800

    design program

--- brain.py
+++ brain.py
@@ -1,9 +1,9 @@
-import turtle
+import turtle              #importing the turtle from standard lib
     
-def draw_square(turtle,length):
+def draw_square(turtle,length):       #drawing square module 
      """this program will draw a pattern"""
      for i in range (4):
-           turtle.forward(length)
+           turtle.forward(length)            #turtle.forward is use to move the pointer forward
            turtle.right(90)
         
         
@@ -11,14 +11,14 @@ def draw_square(turtle,length):
      
              
                     
-def draw_circle(turtle,radius):
-     for i in range(1):
+def draw_circle(turtle,radius):           #drawing circle 
+     for i in range(1):               
          turtle.circle(radius)
                    
               
 def main():
-          brad = turtle.Turtle()
-          brad.shape("turtle")
+          brad = turtle.Turtle()            #creating an instance brad for turtle
+          brad.shape("turtle")          # we can design the pointer for eg:color shape,speed
           brad.color("red")
           brad.speed(8)
           for i in range(40):
